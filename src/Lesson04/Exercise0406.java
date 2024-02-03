4060f67a800881cb0d6ba81ed5226c1970e12357
IvelinaTI <157994294+IvelinaTI@users.noreply.github.com>
2024-02-03 16:01:48 +0200

    Update Exercise 06

--- src/Lesson04/Exercise0406.java
+++ src/Lesson04/Exercise0406.java
@@ -6,6 +6,27 @@ public class Exercise0406 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int num = Integer.parseInt(scanner.nextLine());
+        double five = num % 5;
+//        boolean isTrue = five != 0;
+
+        if (five != 0) {
+            num = Integer.parseInt(scanner.nextLine());
+        } else if (five == 0) {
+            System.out.println(num);
+            num = Integer.parseInt(scanner.nextLine());
+        } else if (num >=150) {
+            return;
+        }
+
+
+//        while (isTrue) {
+//            num = Integer.parseInt(scanner.nextLine());
+//
+//        }
+//        System.out.println(num);
+//        while (num >= 150){
+//            break;
+//        }
     }
 }
