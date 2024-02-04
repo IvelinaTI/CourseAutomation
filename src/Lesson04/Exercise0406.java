206564b610ee5941f7c9e22e70ae76c4224769d4
IvelinaTI <157994294+IvelinaTI@users.noreply.github.com>
2024-02-04 14:54:00 +0200

    Exercise07

--- src/Lesson04/Exercise0406.java
+++ src/Lesson04/Exercise0406.java
@@ -6,27 +6,34 @@ public class Exercise0406 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num = Integer.parseInt(scanner.nextLine());
-        double five = num % 5;
-//        boolean isTrue = five != 0;
 
-        if (five != 0) {
-            num = Integer.parseInt(scanner.nextLine());
-        } else if (five == 0) {
-            System.out.println(num);
-            num = Integer.parseInt(scanner.nextLine());
-        } else if (num >=150) {
-            return;
+        int a = 12;
+        int b = 15;
+        int c = 32;
+        int d = 42;
+        int e = 55;
+        int i = 75;
+        int j = 122;
+        int k = 132;
+        int l = 150;
+        int m = 180;
+        int n = 200;
+
+        if (a % 5 ==0 ){
+            System.out.println(a);
+        }else if (b%5==0) {
+            System.out.println(b);
+        }else if (c%5==0){
+            System.out.println(c);
+        } else if (d %5==0){
+            System.out.println(d);
+        } else if (e % 5 ==0) {
+            System.out.println(e);
+        } else if ( i % 5 ==0) {
+            System.out.println(i);
         }
 
 
-//        while (isTrue) {
-//            num = Integer.parseInt(scanner.nextLine());
-//
-//        }
-//        System.out.println(num);
-//        while (num >= 150){
-//            break;
-//        }
+
     }
 }
---
+++ src/Lesson04/Exercise0407.java
@@ -0,0 +1,16 @@
+package Lesson04;
+
+import java.util.Scanner;
+
+public class Exercise0407 {
+    public static void main(String[] args) {
+
+
+        int i = 50;
+        for (i = 50; i >= 10; i -=10) {
+            System.out.println(i);
+        }
+
+
+    }
+}
